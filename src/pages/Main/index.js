5cb5295b20660d62078337612100fe034f4feb5d
charleseduardome <charleseduardo.me@gmail.com>
2020-03-10 11:23:36 -0300

    Navegação de rotas

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { FaGitAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
@@ -82,7 +83,9 @@ export default class Main extends Component {
           {repositories.map(repository => (
             <li key={repository.name}>
               <span>{repository.name}</span>
-              <a href="teste">Detalhes</a>
+              <Link to={`/repository/${encodeURIComponent(repository.name)}`}>
+                Detalhes
+              </Link>
             </li>
           ))}
         </List>
